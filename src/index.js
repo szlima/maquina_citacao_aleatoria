49aea08a779177e96484f10eaf635e64a8a49f19
limasuu <szlima.93@gmail.com>
2022-11-22 12:08:44 -0300

    Início do projeto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './estilo.css';
+
+ReactDOM.render(
+  <h1>Máquina de citação aleatória</h1>,
+  document.querySelector('#root')
+);
